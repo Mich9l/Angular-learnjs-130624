1bdb97e6112db28821bae83cab8778b298c8d15c
Letto228 <learnjs@MacBook-Pro-Egor.local>
2024-07-12 08:54:42 +0300

    feat: integrate pagination

--- src/app/pages/products-list/products-list.module.ts
+++ src/app/pages/products-list/products-list.module.ts
@@ -1,10 +1,13 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 import {ProductsListComponent} from './products-list.component';
 import {CardModule} from './card/card.module';
 import {IfModule} from '../../shared/if/if.module';
 import {ScrollWithLoadingModule} from '../../shared/scroll-with-loading/scroll-with-loading.module';
+import {PaginationModule} from '../../shared/pagination/pagination.module';
 
 @NgModule({
     declarations: [ProductsListComponent],
@@ -14,6 +17,9 @@ import {ScrollWithLoadingModule} from '../../shared/scroll-with-loading/scroll-w
         MatProgressSpinnerModule,
         IfModule,
         ScrollWithLoadingModule,
+        MatButtonModule,
+        MatIconModule,
+        PaginationModule,
     ],
     exports: [ProductsListComponent],
 })
