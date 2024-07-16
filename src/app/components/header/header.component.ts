e9b7160a292ec98bf169dffb069b88e257022c87
Letto228 <learnjs@MacBook-Pro-Egor.local>
2024-07-16 07:31:49 +0300

    feat: add homework stand

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,11 @@
-import {ChangeDetectionStrategy, Component, EventEmitter, Input, Output} from '@angular/core';
+import {
+    ChangeDetectionStrategy,
+    Component,
+    EventEmitter,
+    Input,
+    Output,
+    TemplateRef,
+} from '@angular/core';
 import {ApplicationConfig} from '../../shared/application-config/application-config.interface';
 
 @Component({
@@ -11,4 +18,12 @@ export class HeaderComponent {
     @Input() applicationConfig: ApplicationConfig | null = null;
 
     @Output() readonly menuClick = new EventEmitter<Event>();
+
+    openPopup(_template: TemplateRef<{$implicit: string}>) {
+        // this.popupService.openPopup(template, context);
+    }
+
+    closePopup() {
+        // this.popupService.closePopup();
+    }
 }
